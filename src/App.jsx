92de2d450b3c4ef60438a89eacd932eb50b501d6
Abel <abelmathew1504@gmail.com>
2025-02-24 23:56:45 +0530

    Added fonts and styling for header

--- src/App.jsx
+++ src/App.jsx
@@ -13,10 +13,10 @@ function App() {
   return (
     <>
       <header className={styles.header}>
-        <Link to="/">Home</Link>
+        <Link className={styles.logoLink} to="/"><img className={styles.logo} src="https://upload.wikimedia.org/wikipedia/commons/e/ec/SCP_Foundation_%28emblem%29.svg" alt="" srcset="" /><h1>SCP Foundation Commissary</h1></Link>
         <div className={styles.nav}>
-          <Link to="store">Store</Link>
-          <Link to="cart"><img className={styles.cartIcon} src="https://banner2.cleanpng.com/20180905/wph/kisspng-computer-icons-portable-network-graphics-clip-art-ic-shopping-cart-px-svg-png-icon-free-download-1-5b907479023c32.0581863815361936570092.jpg" alt="Cart" /> {Object.keys(cartItems).length}</Link>
+          <Link to="store"><h2>Store</h2></Link>
+          <Link className={styles.cartLink} to="cart"><img className={styles.cartIcon} src="https://banner2.cleanpng.com/20180905/wph/kisspng-computer-icons-portable-network-graphics-clip-art-ic-shopping-cart-px-svg-png-icon-free-download-1-5b907479023c32.0581863815361936570092.jpg" alt="Cart" /><h3>{Object.keys(cartItems).length}</h3></Link>
         </div>
       </header>
       <main className={styles.content}>
